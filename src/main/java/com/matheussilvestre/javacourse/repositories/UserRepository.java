5b9476d27e44c530cf7836db0b4ddfda169d9dd2
Matheus Silvestre <mathsylvester13@gmail.com>
2022-03-29 11:14:10 -0300

    JPA repository, dependency injection, database seeding

---
+++ src/main/java/com/matheussilvestre/javacourse/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.matheussilvestre.javacourse.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.matheussilvestre.javacourse.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+
+	
+	
+}
