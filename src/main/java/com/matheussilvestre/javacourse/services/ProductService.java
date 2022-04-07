1567e36143bbd9564a779bd67fd3a3cb0dcc7882
Matheus Silvestre <mathsylvester13@gmail.com>
2022-04-07 10:39:04 -0300

    Product entity, repository, resource and service

---
+++ src/main/java/com/matheussilvestre/javacourse/services/ProductService.java
@@ -0,0 +1,28 @@
+package com.matheussilvestre.javacourse.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.matheussilvestre.javacourse.entities.Product;
+import com.matheussilvestre.javacourse.repositories.ProductRepository;
+
+
+@Service
+public class ProductService {
+
+	@Autowired
+	private ProductRepository repository;
+	
+	public List<Product> findAll(){
+		return repository.findAll();
+	}
+	
+	public Product findById(Long id) {
+		Optional<Product> obj = repository.findById(id);
+		return obj.get();
+	}
+	
+}
